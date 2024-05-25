76b5891616d1247747ce88fe885ff71201f10d12
DharmatejaKathi <153576765+DharmatejaKathi@users.noreply.github.com>
2024-05-25 10:08:33 +0530

    Update index.js

--- src/components/Popular/index.js
+++ src/components/Popular/index.js
@@ -29,7 +29,7 @@ class Popular extends React.Component {
   })
 
   getPopularMoviesResponse = async (page = 1) => {
-    const API_KEY = 'f32b79895b21468afbdd6d5342cbf3db'
+    const API_KEY = 'f32b79895b21468afbdd6d5342cbf3da'
     const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
     const response = await fetch(apiUrl)
     const data = await response.json()
